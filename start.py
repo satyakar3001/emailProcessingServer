88f9fbbe02b85d5ac2a11a0a269ebba1d947fe5e
ManasBehera3 <satyakar3001.com>
2025-08-28 10:59:43 +0530

    Added changes

--- start.py
+++ start.py
@@ -54,7 +54,7 @@ def main():
         import uvicorn
         
         uvicorn.run(
-            app,
+            "app.api:app",
             host="0.0.0.0",
             port=8000,
             reload=True,
